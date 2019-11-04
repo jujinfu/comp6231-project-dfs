0351b8fa8737e972f78f67ed72537a0121b7d524
punkya <punkya007@gmail.com>
2019-11-03 23:05:22 -0500

    Added commons dependency and file reading

--- client/src/main/java/Client.java
+++ client/src/main/java/Client.java
@@ -1,6 +1,9 @@
 import accessServer.AccessServerInterface;
-
+import org.apache.commons.io.FileUtils;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
@@ -8,39 +11,55 @@ public class Client {
     AccessServerInterface accessServer;
     Registry registry;
 
-    public void start() throws Exception{
+    public void start() throws Exception {
         // find the (local) object registry
         registry = LocateRegistry.getRegistry(9999);
         // find the server object
         accessServer = (AccessServerInterface) (registry.lookup("server"));
     }
 
-    public File openFile(String uri) throws Exception{
+    public File openFile(String uri) throws Exception {
+        if (!Files.exists(Paths.get(uri))) {
+            throw new FileNotFoundException("Unable to open file in path " + uri);
+        }
+        return new File(uri);
+    }
 
-        return null;
-    }
-    public boolean saveFile(String uri,byte[] content) throws Exception{
+    public boolean saveFile(String uri, byte[] content) throws Exception {
+       // byte[] content = FileUtils.readFileToByteArray(openFile(uri));
+        accessServer.saveFile(uri, content);
         return false;
     }
-    public boolean closeFile(String uri) throws Exception{
+
+    public boolean closeFile(String uri) throws Exception {
         return false;
     }
-    public boolean deleteFile(String uri) throws Exception{
+
+    public boolean deleteFile(String uri) throws Exception {
         return false;
     }
-    public boolean uploadFile(String uri, File file) throws Exception{
+
+    public boolean uploadFile(String uri, File file) throws Exception {
+        byte[] content = FileUtils.readFileToByteArray(file);
+        //rmiServer.createFile(uri);
         return false;
     }
-    public File downloadFile(String uri) throws Exception{
+
+    public File downloadFile(String uri) throws Exception {
+        //rmiServer.download(uri);
         return null;
     }
-    public String getFileMeta(String uri) throws Exception{
+
+    public String getFileMeta(String uri) throws Exception {
         return null;
     }
-    public String[] listSubDirs(String uri) throws Exception{
+
+    public String[] listSubDirs(String uri) throws Exception {
         return null;
     }
-    public String[] listFiles(String uri) throws Exception{
+
+    public String[] listFiles(String uri) throws Exception {
         return null;
     }
+
 }
